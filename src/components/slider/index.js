6c4bbf43b6377d102c4eca138e994e5c0430ca7a
connecsa <connecsa.com@gmail.com>
2022-10-13 18:13:38 +0530

    newfile

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,36 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from "react-responsive-carousel";
+import {Carousel} from "react-responsive-carousel";
+
 export default function Slider({
-  swipeable,
-  interval,
-  autoPlay,
-  sliders,
-  showThumbs,
-  infiniteLoop,
-  showArrows,
-  showIndicators,
-  centerMode,
-}) {
-  return (
-    <Carousel
-      interval={interval}
-      autoPlay={autoPlay == false ? false : true}
-      centerMode={centerMode == false ? false : true}
-      infiniteLoop={infiniteLoop == false ? false : true}
-      showArrows={showArrows == false ? false : true}
-      showIndicators={showIndicators == false ? false : true}
-      swipeable={swipeable == false ? false : true}
-      showThumbs={showThumbs == true ? true : false}
-    >
-      {sliders.map((i) => (
-        <img className="w-full" src={i.image} />
-      ))}
-    </Carousel>
-  );
+                                   swipeable,
+                                   interval,
+                                   autoPlay,
+                                   sliders,
+                                   showThumbs,
+                                   infiniteLoop,
+                                   showArrows,
+                                   showIndicators,
+                                   centerMode,
+                               }) {
+    return (
+        <Carousel
+            interval={interval}
+            autoPlay={autoPlay == false ? false : true}
+            centerMode={centerMode == false ? false : true}
+            infiniteLoop={infiniteLoop == false ? false : true}
+            showArrows={showArrows == false ? false : true}
+            showIndicators={showIndicators == false ? false : true}
+            swipeable={swipeable == false ? false : true}
+            showThumbs={showThumbs == true ? true : false}
+        >
+            {sliders.map((i) => (
+                <div className="relative"><img className="w-full" src={i.image}/>
+                    <h1 className="absolute top-0 left-4"> Lorem ipsum dolor sit.</h1></div>
+            ))}
+        </Carousel>
+    );
 }
 
 Slider.prototype = {
-  showAr: PropTypes.bool,
+    showAr: PropTypes.bool,
 };
--- src/components/slider/minimogslider/index.js
+++ src/components/slider/minimogslider/index.js
@@ -6,6 +6,8 @@ import Animate from 'animate.css-react'
 import 'animate.css/animate.css'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import Slider from 'react-animated-slider';
+import 'react-animated-slider/build/horizontal.css';
 export default function Minimogslider({
   swipeable,
   interval,
@@ -19,16 +21,33 @@ export default function Minimogslider({
   minimog,Minimodata
 }) {
   return (
-    <Carousel
-        showThumbs={false}
+    <Slider
+        duration={2000}
+        autoplay
+        touchDisabled={true}
+        infinite={true}
     >
       {Minimodata.slider.map((i) => (
-          <img
-              className="w-full  "
-              src={i.image}
-          />
+        <div className="" style={{ background: `url('${i.image}') no-repeat center center` }}
+        >
+
+            {/*<img src=>*/}
+          {/*<img*/}
+          {/*    className="w-full  "*/}
+          {/*    src={i.image}*/}
+          {/*/>*/}
+         <div className="inner  absolute mt-[14%] ml-[8rem] space-y-16">
+           <div><h1 className="absolute text-[13px] md:text-[20px]  top-5 leading-[1.5em] f-[500]">New Arrivals</h1></div>
+           <p className="md:text-[70px] text-[20px] leading-[1.3em]">Clean Vibe <br/>
+             This Summer</p>
+
+           <button className="w-[10rem] rounded-md p-3 mt-[2rem] border border-black">
+             Shop Now
+           </button>
+         </div>
+        </div>
       ))}
-    </Carousel>
+    </Slider>
   );
 }
 
